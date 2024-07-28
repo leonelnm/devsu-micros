be452b9768f055cd3d0c460e5687926763e8034b
Erik <elerickmontenegro@gmail.com>
2024-07-28 16:23:15 -0500

    no se usa

--- commons/src/main/java/com/devsu/commons/exception/RegistroDuplicadoException.java
+++ commons/src/main/java/com/devsu/commons/exception/RegistroDuplicadoException.java
@@ -2,10 +2,6 @@ package com.devsu.commons.exception;
 
 public class RegistroDuplicadoException extends RuntimeException{
 
-    public RegistroDuplicadoException(String message) {
-        super(message);
-    }
-
     public RegistroDuplicadoException(String duplicadoExisteNumeroCuenta, String numero) {
         super(String.format(duplicadoExisteNumeroCuenta, numero));
     }
